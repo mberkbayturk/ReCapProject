f059bdf5be128e5f9cb79276b1f09596894ca55a
Mehmet Berk BAYTÜRK <mberkbayturk@gmail.com>
2021-02-20 18:15:17 +0300

    3 Şubat Ödevi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -15,9 +15,44 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba isminiz en az 2 karakterden oluşmalı ve günlük fıyatı 0'dan büyük olmalıdır.");
+            }
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Update(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba isminiz en az 2 karakterden oluşmalı ve günlük fıyatı 0'dan büyük olmalıdır.");
+            }
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -13,11 +14,11 @@ namespace DataAccess.Concrete.InMemory
         public InMemoryCarDal()
         {
             _cars = new List<Car> {
-                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2014,DailyPrice=360,Description="HYUNDAI"},
-                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=2016,DailyPrice=230,Description="FIAT"},
-                new Car{Id=3,BrandId=2,ColorId=3,ModelYear=2017,DailyPrice=1000,Description="PORSCHE"},
-                new Car{Id=4,BrandId=2,ColorId=1,ModelYear=2018,DailyPrice=200,Description="RENAULT"},
-                new Car{Id=5,BrandId=2,ColorId=2,ModelYear=2016,DailyPrice=560,Description="BMW"},
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2014,DailyPrice=360,Description="Fiat Doblo"},
+                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=2016,DailyPrice=230,Description="Ford Focus"},
+                new Car{Id=3,BrandId=2,ColorId=3,ModelYear=2017,DailyPrice=1000,Description="Renault Megane"},
+                new Car{Id=4,BrandId=2,ColorId=1,ModelYear=2018,DailyPrice=200,Description="Renault Clio"},
+                new Car{Id=5,BrandId=2,ColorId=2,ModelYear=2016,DailyPrice=560,Description="Audi A4"},
             };
         }
         public void Add(Car car)
@@ -31,9 +32,19 @@ namespace DataAccess.Concrete.InMemory
             _cars.Remove(carToDelete);
         }
 
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetAll()
         {
             return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Car> GetById(int carId)
