2fef4b998ef295ffedc1fd542e1b9138548b0c0f
Mehmet Berk BAYTÜRK <mberkbayturk@gmail.com>
2022-08-26 10:05:09 +0300

    Business layer added and tested

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId);
+            }
+        }
+    }
+}
