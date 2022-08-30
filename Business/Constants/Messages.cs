7ada23e3c5c04a5e997fd6d2573f15b9b150fb0a
Mehmet Berk BAYTÜRK <mberkbayturk@gmail.com>
2022-08-30 23:41:35 +0300

    Messages file added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Car added";
+        public static string CarDeleted = "Car deleted";
+        public static string CarUpdated = "Car updated";
+        public static string CarListed = "Car listed";
+        public static string CarCouldNotAdd = "Car could not add";
+        public static string CarCouldNotUpdated = "Car could not update";
+        public static string BrandAdded = "Brand added";
+        public static string BrandUpdated = "Brand updated";
+        public static string BrandDeleted = "Brand deleted";
+        public static string ColorAdded = "Color added";
+        public static string ColorUpdated= "Color updated";
+        public static string ColorDeleted= "Color deleted";
+    }
+}
